6ec70af1e3cbd5537e05f2227bb42f5ddc8b0f63
Jeff Taggart <jeff@jetaggart.com>
2014-10-08 16:13:02 -0600

    Switch to ComponentScan

--- src/main/java/rantly/Main.java
+++ src/main/java/rantly/Main.java
@@ -3,9 +3,11 @@ package rantly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableAutoConfiguration
-@ComponentScan("rantly")
+@ComponentScan
 public class Main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
